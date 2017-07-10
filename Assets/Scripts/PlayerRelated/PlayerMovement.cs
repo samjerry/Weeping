4312dedcd56c0c78e2cf13057d0834dd407a862e
samjerry <jerry@Cant-Touch-This-Macbook.local>
2017-07-10 14:50:23 +0200

    code

---
+++ Assets/Scripts/PlayerRelated/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves and rotates player
+/// </summary>
+
+public class PlayerMovement : MonoBehaviour 
+{
+	private float distance = 3;
+	public float moveSpeed;
+
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+
+	private Rigidbody myRigidBody;
+
+
+	void Start()
+	{
+		myRigidBody = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		{
+			if (Input.GetKey(KeyCode.W))
+			{
+				transform.position = transform.position + Camera.main.transform.forward * distance * Time.deltaTime;
+			}
+
+			if (Input.GetKey(KeyCode.A))
+			{
+				transform.position = transform.position + -Camera.main.transform.right * distance * Time.deltaTime;
+			}
+
+			if (Input.GetKey(KeyCode.S))
+			{
+				transform.position = transform.position + -Camera.main.transform.forward * distance * Time.deltaTime;
+			}
+
+			if (Input.GetKey(KeyCode.D))
+			{
+				transform.position = transform.position + Camera.main.transform.right * distance * Time.deltaTime;
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		myRigidBody.velocity = moveVelocity;
+	}
+}
