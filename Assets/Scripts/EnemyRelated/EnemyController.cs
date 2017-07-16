85af95dcd297e9d0f2af366cbb256f1500e16ee0
MarrowOfAlbion <jerry.sam@ziggo.nl>
2017-07-16 12:06:32 +0200

    Add files via upload
    final Product

---
+++ Assets/Scripts/EnemyRelated/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	private Rigidbody myRB;
+
+	public float moveSpeed;
+
+	public bool IsILOS; 
+	public bool IsActive = false;
+
+	private PlayerMovement thePlayer;
+	private GameObject camera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		camera = GameObject.Find("Main Camera");
+
+		myRB = GetComponent<Rigidbody>();
+		thePlayer = FindObjectOfType<PlayerMovement> ();
+	}
+
+	void Update()
+	{
+
+		Transform[] AngelILOS = camera.GetComponent<FOV> ().visibleTargets.ToArray (); //Zet de list met gameobjects naar een array.
+
+		if (AngelILOS.Length > 0)
+		{
+			Debug.Log (AngelILOS[0].gameObject);
+		}
+
+		transform.position = new Vector3 (transform.position.x, 3.476f, transform.position.z);
+
+		if(AngelILOS.Length > 0)
+		{
+			IsILOS = true;
+		}
+		else
+		{
+			IsILOS = false;
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyRelated/EnemyPathFinding.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPathFinding : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	private GameObject thePlayer;
+
+	private Rigidbody myRB;
+	private EnemyController _ec;
+
+	void Start (){  
+		
+		agent = GetComponent<NavMeshAgent>();  
+		myRB = GetComponent<Rigidbody>();
+		thePlayer = GameObject.Find ("Player");
+		_ec = GetComponent<EnemyController> ();
+	}
+
+	/*
+	void Update(){
+		// *if (!_ec.IsILOS) 
+		{
+			//agent.destination = thePlayer.transform.position;
+		//	agent.SetDestination (thePlayer.transform.position);
+		//	agent.
+		}
+		*
+
+		agent.
+
+	}
+	*/
+
+	void Update()
+	{
+		if (agent.enabled == _ec.IsILOS) 
+			agent.enabled = !_ec.IsILOS;
+
+		if (agent.enabled)
+			agent.SetDestination (thePlayer.transform.position);
+	}
+		
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			myRB.velocity = -transform.forward * 2;
+		}
+	}
+}
+
